dec65679d31febf08f3c0c6c7e1bd0df7f498a16
Ben <ben.401@gmail.com>
2021-12-31 11:21:07 -0800

    js removed

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let newImg;
-newImg = document.getElementById("gifImg").firstChild();
-console.log(newImg);
 
-setTimeout (() => {
-      
-    newImg.appendChild( "<a " + "src = ./imgs/hey.png" + "/>" )
-}, 10000);
 
 
